add5cf4cf784f09229cc246714ac3402ee6683e4
emil0519 <97882056+emil0519@users.noreply.github.com>
2022-08-04 12:34:03 +0800

    Add conditions to script.js

--- week_2_part_4/public/script.js
+++ week_2_part_4/public/script.js
@@ -5,11 +5,29 @@ btn.addEventListener("click", change);
 
 function change() {
   let input = document.querySelector(".num").value;
-  let xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    let response = xhr.responseText;
-    result.innerText = response;
-  };
-  xhr.open("GET", `http://localhost:3000/getData?number=${input}`);
-  xhr.send();
+  if (isNaN(input) == false && input.length !== 0) {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      let response = xhr.responseText;
+      result.innerText = response;
+    };
+    xhr.open("GET", `http://localhost:3000/getData?number=${input}`);
+    xhr.send();
+  } else if (isNaN(input) == true) {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      let response = xhr.responseText;
+      result.innerText = response;
+    };
+    xhr.open("GET", `http://localhost:3000/getData?number=xyz`);
+    xhr.send();
+  } else {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      let response = xhr.responseText;
+      result.innerText = response;
+    };
+    xhr.open("GET", `http://localhost:3000/getData`);
+    xhr.send();
+  }
 }
